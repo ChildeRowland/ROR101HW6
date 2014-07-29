9592caba0606b562cfa12fb3334beeec37e9e173
Christopher Mehmed <chrismehmed@gmail.com>
2014-07-28 21:23:42 -0400

    Comment views and actions

--- HW6/app/controllers/topics_controller.rb
+++ HW6/app/controllers/topics_controller.rb
@@ -7,16 +7,22 @@ class TopicsController < ApplicationController
 		# But this was only allowing for one instance of topic.  I assume because it is the foreign key and not the primary.
 	end
 
+	def show
+		@user = User.find(params[:user_id])
+		@topic = Topic.find(params[:id])
+	end
+
 	def new
 		@user = User.find(params[:user_id])
 		@topic = @user.topics.build(params[:topic])
 	end
 
 	def create
-		@topic = @user.topics.build(params[:topic])
+		@user = User.find(params[:user_id])
+		@topic = @user.topics.build(topic_params)
 		if @topic.save
 			flash[:notice] = "New Topic Added"
-			redirect_to user_path @user
+			redirect_to user_topics_path @user
 		else
 			flash[:notice] = "Topic Not Added"
 			redirect_to new_topic_path
@@ -24,17 +30,36 @@ class TopicsController < ApplicationController
 	end
 
 	def edit
+		@user = User.find(params[:user_id])
+		@topic = Topic.find(params[:id])
+		#@topic = @user.topics.build(params[:topic])
 	end
 
 	def update
+		@user = User.find(params[:user_id])
+		@topic = Topic.find(params[:id])
+		if @topic.update_attributes(topic_params)
+			flash[:notice] = "Topic Updated"
+			redirect_to user_topics_path @user
+		else
+			flash[:notice] = "Something Went Wrong"
+			redirect_to edit_user_topics_path
+		end
 	end
 
 	def destroy
+		if Topic.find(params[:id]).delete
+			flash[:notice] = "Topic Deleted"
+			redirect_to user_topics_path #@user
+		else
+			flash[:notice] = "There was a problem"
+			redirect_to user_topics_path #@user
+		end
 	end
 
 	private   
 	def topic_params    
-		params.require(:topic).permit(:title, :description)   
+		params.require(:topic).permit(:title, :description, :user_id)   
 	end
 
 end
